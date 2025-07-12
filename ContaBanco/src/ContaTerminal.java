6988e43d22da2cf4b5e60c12f3ba6838032ea78d
Igor Fernandes <69645296+igor-cfernandes@users.noreply.github.com>
2025-07-11 21:24:10 -0300

    feat: complete bank account challenge

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\\n");
+        scanner.useLocale(java.util.Locale.US);
+
+        System.out.println("Digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+        
+        String mensagem = 
+            "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, "
+            + "conta %d e seu saldo %.2f já está disponível para saque";
+
+        System.out.printf(mensagem, nome, agencia, conta, saldo);
+
+        scanner.close();
+
     }
 }
